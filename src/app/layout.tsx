6335492765c401d4d157207c83e79a68713e2ba6
thisearlyseason <thisearlyseason@Gmail.com>
2026-07-31 10:53:47 -0600

    Add Meta Pixel tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -201,8 +201,28 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(softwareSchema) }}
         />
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)
+            }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '123960649001817');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body className="font-body antialiased min-h-screen bg-background text-foreground selection:bg-primary/20" suppressHydrationWarning>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=123960649001817&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <FirebaseClientProvider>
           <Suspense fallback={null}>
             <TooltipProvider delayDuration={0}>
